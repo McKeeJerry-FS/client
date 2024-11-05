e4f910606417300d0568fbe825dc723bb0119d48
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-11-05 13:59:52 -0500

    Beginning to work through Navigation

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,13 +1,26 @@
-import Wrapper from '../assets/wrappers/BigSidebar'
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
+import Wrapper from '../assets/wrappers/BigSidebar';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
-const BigSideBar = () => {
-    return (
-        <Wrapper>
-            <div>
-                <h4>BigSideBar</h4>
-            </div>
-        </Wrapper>
-    )
-}
+const BigSidebar = () => {
+  const { showSidebar } = useDashboardContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container ' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default BigSideBar;
+export default BigSidebar;
--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,13 +1,50 @@
-import Wrapper from '../assets/wrappers/SmallSidebar'
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
 
-const SmallSideBar = () => {
-    return (
-        <Wrapper>
-            <div>
-                <h4>SmallSideBar</h4>
-            </div>
-        </Wrapper>
-    )
-}
+import Logo from './Logo';
+import { NavLink } from 'react-router-dom';
+import links from '../utils/links';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
-export default SmallSideBar;
+const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            {links.map((link) => {
+              const { text, path, icon } = link;
+
+              return (
+                <NavLink
+                  to={path}
+                  key={text}
+                  className='nav-link'
+                  onClick={toggleSidebar}
+                  // will discuss in a second
+                  end
+                >
+                  <span className='icon'>{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default SmallSidebar;

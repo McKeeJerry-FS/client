02606d99beb2d602bdfa02236ae16624ef28747c
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-11-05 12:15:54 -0500

    built out the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,29 @@
+import { Logo, FormRow } from '../components'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
 import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-            <Link to="/register">Register Page</Link>
-        </div>
+        <Wrapper>
+            <form className='form'>
+                <Logo />
+                <h4>Login</h4>
+                <FormRow type='email' name='email' defaultValue='jerrymckeejr.dev@gmail.com' />
+                <FormRow type='password' name='password' defaultValue='abc&123!' />
+                <button type='submit' className='btn btn-block'>
+                    Submit
+                </button>
+                <button type='button' className='btn btn-block'>
+                    Explore the App
+                </button>
+                <p>
+                    Not a member yet?
+                    <Link to='/register' className='member-btn'>
+                        Register
+                    </Link>
+                </p>
+            </form>
+        </Wrapper>
     );
 };
 

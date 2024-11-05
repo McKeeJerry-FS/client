e53c94d3ee99644eafc5a625ce71d7e56a116194
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-11-05 12:01:14 -0500

    created a Register page and a FormRow component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,34 @@
 import { Link } from "react-router-dom";
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Logo, FormRow } from '../components';
+
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
-            <Link to="/login">Login Page</Link>
-        </div>
+        <Wrapper>
+            <form className="form">
+                <Logo />
+                <h4>Register</h4>
+                <FormRow type='text' name='name' />
+                <FormRow type='text' name='lastName' labelText='last name' />
+                <FormRow type='text' name='location' />
+                <FormRow type='email' name='email' />
+                
+                <FormRow type='password' name='passwword' />
+                
+
+                <button type="submit" className="btn btn-block">
+                    Submit
+                </button>
+
+                <p>
+                    Already a member?
+                    <Link to='/login' className="member-btn">
+                        Login
+                    </Link>
+                </p>
+            </form>
+        </Wrapper>
     );
 };
 
